766b7fc6cfa6bd343d082a8d736445ae151da625
Luciano Macedo <luciano.macedo@duo.studio>
2023-08-13 03:42:36 -0300

    adiciona views e rotas

---
+++ resources/views/site/sobre-nos.blade.php
@@ -0,0 +1,1 @@
+<h3>sobre-nos</h3>
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PrincipalController::class, 'index']);
+Route::get('/', PrincipalController::class);
 
-Route::get('/sobre-nos', [SobreNosController::class, 'index']);
+Route::get('/sobre-nos', SobreNosController::class);
 
-Route::get('/contato', [ContatoController::class, 'index']);
+Route::get('/contato', ContatoController::class);
