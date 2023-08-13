144ef1aa779db38b827d5042ca9921a9c9642b80
Luciano Macedo <luciano.macedo@duo.studio>
2023-08-13 04:24:28 -0300

    novas rotas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\PrincipalController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\FornecedorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,8 +17,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', PrincipalController::class);
+Route::get('/', PrincipalController::class)->name('Site.Index');
 
-Route::get('/sobre-nos', SobreNosController::class);
+Route::get('/sobre-nos', SobreNosController::class)->name('Site.Sobre-nos');
 
-Route::get('/contato', ContatoController::class);
+Route::get('/contato', ContatoController::class)->name('Site.Contato');
+
+Route::prefix('/app')->group(function(){
+    Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+    Route::get('/fornecedores', FornecedorController::class)->name('app.fornecedores');
+    Route::get('/produtos', function(){return 'produtos';})->name('app.produtos');
+});
+
+Route::fallback(function() {
+    echo 'A rota acessada não existe. <a href="'.route('site.index').'">clique aqui</a> para ir para página inicial';
+});
